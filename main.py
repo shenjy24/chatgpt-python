e87b9e4f37d4ec0041ef24e9a0ed492d35eff1f5
shenjy <shenjy24@gmail.com>
2023-03-02 15:35:28 +0800

    init

---
+++ main.py
@@ -0,0 +1,28 @@
+import openai
+import tiktoken
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
+
+def chatgpt(content):
+    """调用OpenAi的聊天接口"""
+    openai.api_key = ""
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": content}])
+    message = completion.choices[0].message.content
+    print(message)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(num_tokens_from_string("tiktoken is great!", "gpt2"))
+    print(chatgpt("pip常用指令"))
---
+++ readme.md
@@ -0,0 +1,5 @@
+### 获取字符串的token数
+参考文档：[How to count tokens with tiktoken](https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb)
+
+### ChatGPT聊天
+参考文档：[Create chat completion](https://platform.openai.com/docs/api-reference/chat/create?lang=python)
