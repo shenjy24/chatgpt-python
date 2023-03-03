2d15076fd7c880ecb2a4b6ae1024885b94de5e13
shenjy <shenjy24@gmail.com>
2023-03-03 20:05:32 +0800

    聊天

--- main.py
+++ main.py
@@ -22,7 +22,34 @@ def chatgpt(content):
     print(message)
 
 
+def chat():
+    openai.api_key = ''
+
+    with open('chat.txt', 'a', encoding="utf-8") as f:
+        while True:
+            # 获取用户输入
+            user_input = input("You: ")
+
+            # 将用户输入写入文件
+            f.write("You: " + user_input + "\n")
+
+            # 判断用户是否输入 "exit"，如果是则退出聊天
+            if user_input == "exit":
+                break
+
+            completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                      messages=[{"role": "user", "content": user_input}])
+            message = completion.choices[0].message.content
+
+            # 输出回复到命令行
+            print(message)
+
+            # 将回复写入文件
+            f.write("Bot: " + message + "\n")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(num_tokens_from_string("tiktoken is great!", "gpt2"))
-    print(chatgpt("pip常用指令"))
+    # print(num_tokens_from_string("tiktoken is great!", "gpt2"))
+    # print(chatgpt("pip常用指令"))
+    chat()
